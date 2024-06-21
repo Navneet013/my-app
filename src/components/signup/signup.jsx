bfd86e9b301c19cf6fcada8ce20f0c7182e0b2f9
Navneet013 <imnavneet509@gmail.com>
2024-06-21 05:48:15 +0530

    second

---
+++ src/components/signup/signup.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './signup.css'
+export default function signup() {
+  return (
+    <div className='whole'>
+        <div className="whole1">
+            <div className="whole2">
+                <div className="map">
+<h2 className='txt400'>Sign Up</h2>
+<div className="logo400">
+    
+</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
